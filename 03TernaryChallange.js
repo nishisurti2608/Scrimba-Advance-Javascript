6df5d74197121198ad9bdcc19959eca09b78e4dd
Nishi Surti <70151642+nishisurti2608@users.noreply.github.com>
2026-03-18 20:38:28 +0530

    Ternary Challanges

---
+++ 03TernaryChallange.js
@@ -0,0 +1,19 @@
+const playerGuess = 6
+const correctAnswer = 6
+
+/*
+Challenge
+1. Now improve the functionality of this code by 
+   letting the player know if their guess was too high, 
+   too low, or exactly right.
+*/
+
+// const message = playerGuess < correctAnswer ? 'Correct!' : 'Wrong!'
+
+
+const message = playerGuess < correctAnswer ? 'Too low!' 
+    : playerGuess > correctAnswer ? 'Too high' 
+    : 'Exactly right!'
+
+
+console.log(message)
