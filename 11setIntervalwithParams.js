fa85c869ac69700e4745ee0748ce6b8707b69bb4
Nishi Surti <70151642+nishisurti2608@users.noreply.github.com>
2026-03-30 17:48:30 +0530

    setinterval with param  challange

---
+++ 11setIntervalwithParams.js
@@ -0,0 +1,15 @@
+function startCountdown(device) {
+    let secondsRemaining = 3
+
+    const shutdownTimer = setInterval(function() {
+        if (secondsRemaining > 0) {
+            console.log(`Your ${device} will shut down in ${secondsRemaining} seconds`)
+            secondsRemaining--
+        } else {
+            console.log(`Your ${device} is shutting down`)
+            clearInterval(shutdownTimer)
+        }
+
+    }, 1000, 'MacBook')
+}
+startCountdown('MacBook')
