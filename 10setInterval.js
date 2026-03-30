af4d835c1b4261b6a1d898bd8c9dc9002a99c2bd
Nishi Surti <70151642+nishisurti2608@users.noreply.github.com>
2026-03-30 17:36:09 +0530

    setinterval challange

---
+++ 10setInterval.js
@@ -0,0 +1,23 @@
+function startCountdown() {
+    let secondsRemaining = 3
+    
+    const shutdownTimer = setInterval(function(){
+        console.log(`Your device will shut down in ${secondsRemaining} seconds`)
+        secondsRemaining--
+        if (secondsRemaining===0 ){
+            console.log( `Your device is shutting down`)
+            clearInterval(shutdownTimer)
+            } 
+    },1000)
+/*
+Challenge:
+1. Every 1000 milliseconds, log out: 
+    `Your device will shut down in ${secondsRemaining} seconds`.
+    Decrement 'secondsRemaining'.
+2. When 'secondsRemaining' hits zero, you should log out 
+    `Your device is shutting down`. 
+    At this point, the setInterval should be cleared. What method 
+    can you use to do that? (We used clearTimeout to cancel setTimeout.)
+*/
+}
+startCountdown()
