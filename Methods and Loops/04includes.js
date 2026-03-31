09d4d6f620d09a9491ddb3b75f6e0cc47eb9fd30
Nishi Surti <70151642+nishisurti2608@users.noreply.github.com>
2026-03-31 22:35:40 +0530

    for each

---
+++ Methods and Loops/04includes.js
@@ -0,0 +1,5 @@
+/* A method for checking if array holds a given value  */
+
+
+const emojis = ['🐥','🐯','🐼']
+console.log(emojis.includes('🐴'))
---
+++ Methods and Loops/06join.js
@@ -0,0 +1,12 @@
+/* join is inbuilt javascript method for creating strings from arrays
+
+The join() method:
+Concatenates elements of array into a string
+You choose how elements are separated
+Returns the new string
+
+*/
+
+const guestsArr = ['Amy', 'Clare', 'Keith', 'Dan'] 
+
+console.log(guestsArr.join(' '))
