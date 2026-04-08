835b5de5c3fe0ca665b9458314414987f1b824b9
Nishi Surti <70151642+nishisurti2608@users.noreply.github.com>
2026-04-08 18:26:49 +0530

    objects

---
+++ 06Objects/01InbuiltMethod.js
@@ -0,0 +1,10 @@
+// Static method live on constructor 
+
+const books = {
+  "b001": { title: "To Kill a Mockingbird", price: 18.99, isAvailable: true },
+  "b002": { title: "1984", price: 15.99, isAvailable: false },
+  "b003": { title: "The Great Gatsby", price: 12.49, isAvailable: true },
+  "b004": { title: "Moby Dick", price: 22.50, isAvailable: false }
+}
+
+console.log(Object.keys(books))
